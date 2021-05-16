89e81039a3759a3d1d8cdd538dc12591ea2629f3
Stacy Harrison <stacy.e.harrison@gmail.com>
2021-05-16 15:04:34 -0400

    Add blog title to page title and SEO

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import Layout from './layout'
+import SEO from './seo';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const Posts = ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+    {
+      allMdx {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  let title = data.allMdx.edges[0].node?.frontmatter?.title || 'Blog';
+
   return (
     <Layout>
+      <SEO title={title} />
       {children}
     </Layout>
   );
