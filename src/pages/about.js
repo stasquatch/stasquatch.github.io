a6056335134b77eb3dc1df88bbd9353c33c541f2
Stacy Harrison <stacy.e.harrison@gmail.com>
2021-05-16 16:51:13 -0400

    Add skills list

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import LogoRow from '../components/logo-row';
+import SkillsList from '../components/skills-list';
 
 const AboutPage = () => {
   return (
@@ -12,8 +13,38 @@ const AboutPage = () => {
       <h2>Around the Internet</h2>
       <LogoRow />
 
+      <h2>Skills</h2>
+      <SkillsList skills={skills} />
+
     </Layout>
   )
 }
+
+const skills = [
+  'JavaScript',
+  'Ember.js',
+  'React Ruby on Rails',
+  'RSpec HTML5',
+  'CSS3',
+  'JS/ES6+ Node',
+  'Next.js',
+  'Express.js',
+  'QUnit',
+  'Jest',
+  'C#',
+  'ASP.NET',
+  'Bootstrap',
+  'Bulma',
+  'WCAG 2.0 AA CompSkillItemance',
+  'WAVE',
+  'Axe',
+  'Jaws screen reader',
+  'BDD',
+  'Cucumber',
+  'Selenium',
+  'Adobe Creative Suite',
+  'Web & Content Copywriting',
+  'Google Analytics/Webmaster Tools'
+];
 
 export default AboutPage;
