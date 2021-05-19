c82ea4c46a0c1ac50082acd76ac6326a30adaef0
Stacy Harrison <stacy.e.harrison@gmail.com>
2021-05-19 08:00:51 -0400

    add category filtering based on query param

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,27 +3,39 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from 'gatsby'
 import PostPreview from '../components/post-preview'
+import { useQueryParam, StringParam } from 'use-query-params';
 
 const IndexPage = (props) => {
+  const [category] = useQueryParam('category', StringParam);
+
   let posts = props.data.allMdx.edges;
+
+  // client-side filtering until we can figure out how to refactor graphql-gatsby to accept query param values
+  let filteredPosts = category ? posts.filter(post => post.node.frontmatter.category === category) : posts;
 
   return (
     <Layout>
       <SEO title="Home" />
-      {posts.map(post => <PostPreview key={post.node.id} post={post.node} />)}
+      {filteredPosts.map(post => <PostPreview key={post.node.id} post={post.node} />)}
     </Layout>
   )
 };
 
 export const query = graphql`
   {
-    allMdx {
+    allMdx(
+      sort: {
+        fields: [frontmatter___date]
+        order: ASC
+      }
+    ) {
       edges {
         node {
           id
           frontmatter {
             date(formatString: "MMMM Do, YYYY")
             title
+            category
           }
           excerpt
           timeToRead
